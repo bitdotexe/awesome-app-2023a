9b9cfd74773c21449772248776a8a0ab245ff843
UrielTG <uriel09890@gmail.com>
2023-05-12 16:05:38 +0000

    Adding a new "page" called about and adding css

--- app.js
+++ app.js
@@ -7,6 +7,45 @@ import express from "express";
 // Creating an Express instance
 const app = express(); // (req, res) => { CODE }
 
+// Middleware de proposito específico
+app.use("/about", (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap"
+        rel="stylesheet"
+      />
+      <style>
+        * {
+          font-family: "Josefin Sans", sans-serif;
+        }
+
+        body {
+          background-color: #333;
+        }
+
+        h1, p {
+          color: #FFFF;
+        }
+      </style>
+      <title>About</title>
+    </head>
+    <body>
+      <h1>This proyect was coded by: Bit.exe </h1>
+      <p>This is a page created to learn Fullstack web development with JS</p>
+      <p>Uriel Abisai Torres García | 191130325</p>
+    </body>
+  </html>
+  `);
+});
+
 // Registering our first middleware
 app.use((req, res, next) => {
   console.log("⚙️  Executing Middleware #1");
@@ -38,11 +77,19 @@ app.use((req, res) => {
         * {
           font-family: "Josefin Sans", sans-serif;
         }
+
+        body {
+          background-color: #333;
+        }
+
+        h1, p {
+          color: #FFFF;
+        }
       </style>
       <title>ExpressJS app</title>
     </head>
     <body>
-      <h1>Welcome to ExpressJS</h1>
+      <h1><b>Welcome to ExpressJS</b></h1>
       <p>This is my awesome app</p>
     </body>
   </html>
